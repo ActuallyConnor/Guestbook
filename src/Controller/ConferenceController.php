6abaacad311886cd6c2ab82f15e029a85343829b
ActuallyConnor <connor.smyth@raceroster.com>
2021-08-05 14:40:08 -0400

    Able to add conferences but unable to add comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,25 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(
-        string $name
-    ): Response {
-        $greet = '';
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
-
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
         return new Response(<<<EOF
 <html>
     <body>
-        $greet
-        <img src="/images/under-construction.gif"/>
+        <img src="/images/under-construction.gif" />
     </body>
 </html>
 EOF

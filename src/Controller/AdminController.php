63d09c2cf259863830ffeaf0a664f7c01269d6f1
connor <connorsmyth@rogers.com>
2021-09-14 09:34:09 -0400

    Grouping similar routes with a prefix

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -15,6 +15,7 @@ use Symfony\Component\Workflow\Registry;
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Route('/admin')]
 class AdminController extends AbstractController
 {
     private $twig;
@@ -28,7 +29,7 @@ class AdminController extends AbstractController
         $this->bus           = $bus;
     }
 
-    #[Route('/admin/comment/review/{id}', name: 'review_comment')]
+    #[Route('/comment/review/{id}', name: 'review_comment')]
     public function reviewComment(
         Request $request,
         Comment $comment,
@@ -58,7 +59,7 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/admin/http-cache/{uri<.*>}', methods: ['PURGE'])]
+    #[Route('/http-cache/{uri<.*>}', methods: ['PURGE'])]
     public function purgeHttpCache(KernelInterface $kernel, Request $request, string $uri): Response
     {
         if ('prod' === $kernel->getEnvironment()) {
